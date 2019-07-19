eba1b97d66d021049bcff754d80f10ce4f2a8903
Wei-Shiang Lian <weishianglian@gmail.com>
2019-07-19 13:27:10 +0100

    Use "class" to implement object concept

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,42 @@ person.changeName('Edward').changeAge('22').changeJob(['Ruby on Rails','Java', '
 
 document.querySelector('.test').innerHTML = person.name + ' is ' + person.age + ' years old and doing ' + person.job.join(', ');
 document.querySelector('.test').style.fontFamily = 'helvetica';
+
+
+
+// Use class to instantiate objects
+class Person {
+    constructor(name, age, job) {
+        this.name = name
+        this.age = age
+        this.job = job
+    }
+    // Attribute setters
+    name(name) { this.name = name }
+    age(age) { this.age = age }
+    job(job) { this.job = job }
+
+    // Display function to compose the string
+    greet() {
+        return `${this.name} is ${this.age} years old and doing ${this.job.join(", ")}`
+    }
+}
+
+// Instantiate person with constructor
+let person1 = new Person("Belle", "18", ['Social Design','Website Design','User Interface Design','Front-end Design'])
+let person2 = new Person("Edward", "22", ['Ruby on Rails','Java', 'Machine Learning'])
+
+// Create <p> and be appended to the div woth class called test2
+let p1 = document.createElement("P"), p2 = document.createElement("P")
+p1.innerText = person1.greet()
+p2.innerText = person2.greet()
+document.querySelector('.test2').appendChild(p1).appendChild(p2)
+document.querySelector('.test2').style.fontFamily = 'helvetica'
+
+// Modify the attributes
+person2.name = "WS Lian"
+person2.age = 24
+person2.job = ['Ruby on Rails', 'Java', 'Python', 'Big Data', 'Machine Learning', 'Computer Vision']
+let p3 = document.createElement("P")
+p3.innerText = person2.greet()
+document.querySelector('.test2').appendChild(p3)
